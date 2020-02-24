39e41567c342da98a8d78011731a8ee7c30890e6
Niels Dreijer <hello@dreijer.io>
2020-02-24 10:25:41 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const toggleOff = document.querySelector(".toggle-off");
+const toggleOn = document.querySelector(".toggle-on");
+
+toggleOff.addEventListener("click", function() {
+  toggleOff.classList.toggle("toggle-off");
+  toggleOff.classList.toggle("toggle-on");
+});
+
+toggleOn.addEventListener("click", function() {
+  toggleOn.classList.toggle("toggle-off");
+  toggleOn.classList.toggle("toggle-on");
+});
