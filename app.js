8eea38a920b4faa13da0e41072b0631a6792c693
Niels Dreijer <hello@dreijer.io>
2020-02-27 21:02:30 +0100

    added difficulty functionality

--- app.js
+++ app.js
@@ -10,10 +10,20 @@ const difficultyArrowRight = document
 const difficultySelector = document
   .getElementsByClassName("single-setting")[0]
   .getElementsByClassName("value-text")[0];
+const difficultyParry = document
+  .getElementsByClassName("difficulty-progress-bar")[0]
+  .getElementsByClassName("difficulty-progress-bar-fill")[0];
+const difficultyIncDamage = document
+  .getElementsByClassName("difficulty-progress-bar")[1]
+  .getElementsByClassName("difficulty-progress-bar-fill")[0];
+const difficultyEnemyAggression = document
+  .getElementsByClassName("difficulty-progress-bar")[2]
+  .getElementsByClassName("difficulty-progress-bar-fill")[0];
 
 const toggleOff = document.querySelector(".toggle-off");
 const toggleOn = document.querySelector(".toggle-on");
 
+// TOGGLE BUTTONS
 toggleOff.addEventListener("click", function() {
   toggleOff.classList.toggle("toggle-off");
   toggleOff.classList.toggle("toggle-on");
@@ -26,12 +36,36 @@ toggleOn.addEventListener("click", function() {
 
 // DIFFICULTY SELECTOR
 // array for each difficulty
-let difficulty = [
+const difficulty = [
   "Story Mode",
   "Jedi Knight",
   "Jedi Master",
   "Jedi Grand Master"
 ];
+
+// object with all progression bars length (in pixels) - used when changing difficulty
+const allDifficulties = {
+  storyMode: {
+    parryTiming: 165,
+    incDamage: 30,
+    enemyAggression: 30
+  },
+  jediKnight: {
+    parryTiming: 130,
+    incDamage: 50,
+    enemyAggression: 70
+  },
+  jediMaster: {
+    parryTiming: 85,
+    incDamage: 85,
+    enemyAggression: 85
+  },
+  jediGrandMaster: {
+    parryTiming: 30,
+    incDamage: 165,
+    enemyAggression: 165
+  }
+};
 
 // change difficulty up
 function changeDifficultyUp() {
@@ -41,6 +75,7 @@ function changeDifficultyUp() {
   } else {
     difficultySelector.innerHTML = difficulty[0];
   }
+  difficultyIndicators();
 }
 
 // change difficulty down
@@ -51,8 +86,21 @@ function changeDifficultyDown() {
   } else {
     difficultySelector.innerHTML = difficulty[3];
   }
+  difficultyIndicators();
 }
 
+// change the difficulty progress bars depending on which difficulty is selected
+//
+function difficultyIndicators() {
+  const difficultyKeys = Object.keys(allDifficulties)[
+    difficulty.indexOf(difficultySelector.innerHTML)
+  ];
+  difficultyParry.style.width = `${allDifficulties[difficultyKeys].parryTiming}px`;
+  difficultyIncDamage.style.width = `${allDifficulties[difficultyKeys].incDamage}px`;
+  difficultyEnemyAggression.style.width = `${allDifficulties[difficultyKeys].enemyAggression}px`;
+}
+
+// change difficulty right - both click and key
 difficultyArrowRight.addEventListener("click", function() {
   changeDifficultyUp();
 });
@@ -63,6 +111,7 @@ window.addEventListener("keydown", function(e) {
   }
 });
 
+// change difficulty left - both click and key
 difficultyArrowLeft.addEventListener("click", function() {
   changeDifficultyDown();
 });
